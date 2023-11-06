6174651a8eecf1a87fd1380839febab9ea57b9d7
Tim Diliberto <timothydiliberto19@gmail.com>
2023-11-05 18:56:01 -0800

    Added comments

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,18 @@
+/*
+hello.cpp
+Prints a greeting followed by personal details
+
+Tim Diliberto
+*/
 #include <cstdio>
 
 int main()
 {
+    // prints a greeting and my name
     printf("Hello world! My name is Tim.\n");
+    // prints my location
     printf("I live in San Jose.\n");
+    //prints my passion
     printf("I love to code!\n");
     return 0;
 }
